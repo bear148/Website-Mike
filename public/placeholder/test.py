4f1a97874677b3a55e76c5214d55a52dc53db3e3
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-02 13:34:37 -0700

    Added a py file

--- public/placeholder/test.py
+++ public/placeholder/test.py
@@ -1,5 +1,9 @@
-def func(message):
-    print(message)
-    print(message)
-    print(message)
-    print(message)
+from time import sleep
+total = 30
+
+for perc in range(101):
+    num = total * perc //100
+    done = ">" * num
+    todo = "-" * (total - num)
+    print('\r', f'{perc:02}', '% ', done+todo, sep='', end='', flush=True)
+    sleep(0.02)
