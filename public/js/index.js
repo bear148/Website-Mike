45f587399946a4a65be81c72092e31f4e98673c5
BizzyPythonBear <bigfatharpseal@gmail.com>
2021-06-02 12:54:28 -0700

    Initial Commit

---
+++ public/js/index.js
@@ -0,0 +1,5 @@
+function multiply(x, y) {
+    return x + y;
+};
+
+console.log(multiple(5,6));
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/html', 'index.html'));
+    app.use('/public/css', express.static(__dirname + '/public/css'));
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on localhost3000");
+});
