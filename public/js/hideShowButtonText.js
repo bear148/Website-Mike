482de768717be160d188e2cc36cc88e1dfa36ae7
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-02 13:49:38 -0700

    Fixed Image Bug

--- public/js/hideShowButtonText.js
+++
@@ -1,7 +0,0 @@
-function firstFunc() {
-    document.getElementById("test").innerText = "Go to my github to view my projects!"
-    document.getElementById("gitLink").innerText = "--Github Page--"
-}
-function displayBio() {
-    document.getElementById("bioText").innerText = "Hello there, my name is Michael S, and I'm an aspiring developer. My favorite languages to work with include python, javascript, html, css, lua, and kotlin. I first got into programming when I wanted to make a game. Though, after setting this goal so ealry on, I quickly realized that this was to big of a goal, so I set out to make something more reasonable, which was a simple hello world program in the language I still love to this day, python."
-}
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/html', 'index.html'));
     app.use('/public/css', express.static(__dirname + '/public/css'));
+    app.use('/public/images', express.static(__dirname + '/public/images'));
 });
 
 app.listen(3000, function () {
