9c838c7d1d11ff4486a8a66ff06c80ab7f3664ca
BizzyPythonBear <bizzeebear@hotmail.com>
2021-06-06 17:51:42 -0700

    Testing

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/html', 'index.html'));
     app.use('/public/css', express.static(__dirname + '/public/css'));
     app.use('/public/images', express.static(__dirname + '/public/images'));
+    app.use('/public/html', express.static(__dirname + '/public/html'));
 });
 
 app.listen(3000, function () {
